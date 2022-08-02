a2a6754dad579fb25e00f24bc54e2a03321da910
aodak9 <aodak9@github.com>
2022-08-02 17:28:57 -0500

    feat(src/components/dumbComponents/Card) EmilioBranch Card creada

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from './components/dumbComponents/NavBar/NavBar';
+import Card from './components/dumbComponents/Card/Card';
 function App() {
   return (
     <div className='w-screen h-screen bg-greyBlack-100'>
       <NavBar/>
       <h1>Imagenes paginados</h1>
       <h1>Componentes con libros</h1>
+      <Card/>
       <h1>Paginados con imagenes con categorias</h1>
       <h1>Libro destacado de la semana</h1>
       <h1>Footer</h1>
