c92b9beadf51a8ee86c9793da361fd89426ee5d0
Kevin Lewicki <kevin@mobilityps.com>
2022-08-03 03:00:13 -0300

    feat(src/components/Footer): Nuria realice el footer y agregue en app

--- src/components/smartComponents/Filters/Filters.jsx
+++ src/components/smartComponents/Filters/Filters.jsx
@@ -5,9 +5,9 @@ let arrayYears = [1992, 1230, 2001, 2015, 2017]
 export default function Filters() {
   return (
     <div className='grid h-full grid-cols-3 pt-1 mx-40 justify-items-center'>
-       <Filter text={'Genero'} filtros={arrayGeres}/>
-       <Filter text={'Escritor'}/>
-       <Filter text={'Año'} filtros={arrayYears}/>
+        <Filter text={'Genero'} filtros={arrayGeres}/>
+        <Filter text={'Escritor'}/>
+        <Filter text={'Año'} filtros={arrayYears}/>
     </div>
   )
 }
