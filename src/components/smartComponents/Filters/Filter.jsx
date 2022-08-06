b6197e65cfaef896829bd09e177e74c7e4f2fd60
Heider <heiderlopez123@gmail.com>
2022-08-06 10:57:11 -0500

    front(limpieza de codigo)

--- src/components/smartComponents/Filters/Filter.jsx
+++ src/components/smartComponents/Filters/Filter.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import {getBooksGenres} from '../../../redux/actions/actions'
-export default function Filter({text, filtros}) {
+import { getBooksGenres } from '../../../redux/actions/actions'
+export default function Filter({ text, filtros }) {
   let navigate = useNavigate()
   let dispatch = useDispatch()
-  let handleOnClick = (e)=>{
+  let handleOnClick = (e) => {
     dispatch(getBooksGenres(e.target.id))
     navigate(`/categoria/${e.target.id}`)
   }
   return (
     <div className='w-80 z-10'>
-      <details class="open:bg-cream-300 open:shadow-lg rounded-lg p-2">
-          <summary class="text-sm text-greyBlack-400 font-semibold select-none p-1">
-            {text}
-          </summary>
-        <div class="p-2 grid grid-cols-2 gap-1 text-sm leading-6 text-greyBlack-400 border-t-2 border-greyBlack-100">
+      <details className="open:bg-cream-300 open:shadow-lg rounded-lg p-2">
+        <summary className="text-sm text-greyBlack-400 font-semibold select-none p-1">
+          {text}
+        </summary>
+        <div className="p-2 grid grid-cols-2 gap-1 text-sm leading-6 text-greyBlack-400 border-t-2 border-greyBlack-100">
           {
-            filtros && filtros.map((e)=> <button key={e.id} id={e.genre} onClick={(e)=>handleOnClick(e)}>{e.genre}</button>)
+            filtros && filtros.map((e) => <button key={e.id} id={e.genre || e.released} onClick={(e) => handleOnClick(e)}>{e.genre || e.released}</button>)
           }
         </div>
       </details>
--- src/redux/reducers/books.js
+++ src/redux/reducers/books.js
@@ -1,10 +1,12 @@
-import {GET_ALL_BOOKS, GET_BOOK_BY_ID, GET_BOOKS_BY_NAME, GET_BOOK_BY_GENRE} from '../actions/variables'
+import {GET_ALL_BOOKS, GET_BOOK_BY_ID, GET_BOOKS_BY_NAME, GET_BOOK_BY_GENRE, GET_YEARS} from '../actions/variables'
 
 let initialState = {
     allBooks: [],
     allBooksByName: [],
     allBooksByGenre: [],
     bookById: {},
+    year: [],
+    name: ''
 }
 
 export default function root(state = initialState, actions){
@@ -22,12 +24,24 @@ export default function root(state = initialState, actions){
         case GET_BOOKS_BY_NAME:
             return {
                 ...state,
-                allBooksByName: actions.payload
+                allBooksByName: actions.payload.Books,
+                name: actions.payload.name
             }
         case GET_BOOK_BY_GENRE:
             return {
                 ...state,
                 allBooksByGenre: actions.payload
+            }
+        case GET_YEARS:
+            let years = actions.payload.map((l, i) => {
+                return {
+                    id: i,
+                    released: l.released
+                }
+            })
+            return {
+                ...state,
+                year: years
             }
         default:
             return {...state}
